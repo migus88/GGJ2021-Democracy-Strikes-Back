ac38e183f3b1b81139058884c3c78e26700b4e35
Yura Sokolov <migsil88@gmail.com>
2021-01-29 10:16:44 +0200

    Player navigation in progress

---
+++ Assets/Bootstrap/_SubDomains/Battle/Code/Installers/BattleInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class BattleInstaller : MonoInstaller
+{
+    [SerializeField] private InputManager _inputManager;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<InputManager>().FromInstance(_inputManager).AsSingle();
+    }
+}
---
+++ Assets/Bootstrap/_SubDomains/Battle/Code/Managers/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public event Action<Character> CharacterDragStarted;
+    public event Action<Transform> TileHovered;
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] private LayerMask _playerMask;
+    [SerializeField] private LayerMask _tileMask;
+    [SerializeField] private float _maxRayDistance = 250f;
+    
+    
+    
+    private void Update()
+    {
+        var hoveredTileTransform = FindHoveredTile();
+
+        if (hoveredTileTransform)
+        {
+            TileHovered?.Invoke(hoveredTileTransform);
+        }
+    }
+
+    private Transform FindHoveredTile()
+    {
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, _maxRayDistance, _tileMask))
+        {
+            return hit.transform;
+        }
+
+        return null;
+    }
+}
