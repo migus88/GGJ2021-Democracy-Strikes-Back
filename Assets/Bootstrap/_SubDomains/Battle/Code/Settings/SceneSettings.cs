d2079a8d7d06ad305aba3a0c11c1c8229e6ed0e9
Roman Raspik <raspik@gmail.com>
2021-01-29 12:22:08 +0200

    wtf?Stashad

---
+++ Assets/Bootstrap/_SubDomains/Battle/Code/Settings/SceneSettings.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Bootstrap._SubDomains.Battle.Code.Settings
+{
+    [CreateAssetMenu(fileName = "Scene", menuName = "Atomic/Settings", order = 0)]
+    public class SceneSettings : ScriptableObject
+    {
+        public AssetReferenceT<SceneAsset> MainScene;
+        public AssetReferenceT<SceneAsset>[] DependencyScenes;
+
+        [System.Serializable]
+        public class SceneAssetReference : AssetReferenceT<SceneAsset>
+        {
+            public SceneAssetReference(string guid) : base(guid)
+            {
+            }
+        }
+    }
+}
---
+++ Assets/Bootstrap/_SubDomains/Battle/Code/Settings/SceneSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Bootstrap._SubDomains.Battle.Code.Interfaces;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Bootstrap._SubDomains.Battle.Code.Settings
+{
+    public class SceneSystem : MonoBehaviour 
+    {
+        //
+        private SceneSettings.SceneAssetReference[] _currentlyLoadedScenes;
+        public async UniTask LoadScenes(SceneSettings settings)
+        {
+            var tasks = new List<UniTask>();
+
+            foreach (var scene in settings.DependencyScenes) 
+            {
+                tasks.Add(Addressables.LoadSceneAsync(scene, LoadSceneMode.Additive).ToUniTask());
+            }
+
+            await UniTask.WhenAll(tasks);
+        }
+
+        public async void UnloadAllScenes()
+        {
+            var tasks = new List<UniTask>();
+            foreach (var scene in _currentlyLoadedScenes)
+            {
+                tasks.Add(Addressables.UnloadSceneAsync(scene.OperationHandle).ToUniTask());
+            }
+            
+            await UniTask.WhenAll(tasks);
+        }
+
+        public void UnloadScene()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
